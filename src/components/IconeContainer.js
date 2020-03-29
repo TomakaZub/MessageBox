2c6555bbd1f17ab078017e0b241df3e0eca2dc6a
Thomas <thomas_aguirregabiria@live.fr>
2020-03-29 22:17:20 +0200

    update color already liked

--- src/components/IconeContainer.js
+++ src/components/IconeContainer.js
@@ -3,7 +3,7 @@ import React from "react"
 const IconeContainer = ({ children, color, count, isLike, ...props }) => {
   // ...props le reste des props
   return (
-    <div className={`icon-group ${isLike && "icon-islike"}`} {...props}>
+    <div className={`icon-group ${isLike && "icon-isLike"}`} {...props}>
       <div className={`icon-container icon-${color}`}>{children}</div>
       {count > 0 && <p className={`icon-text icon-text-${color}`}>{count}</p>}
     </div>
